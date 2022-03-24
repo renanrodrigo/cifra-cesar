586c05ca96ee0b307b12808e43cdfefd7784e243
Renan Rodrigo <renan@linuxmail.org>
2022-03-24 19:38:52 -0300

    Juntar as funcoes de criptografia em uma so

--- cifra-cesar.js
+++ cifra-cesar.js
@@ -1,44 +1,35 @@
 const funcao_criptografar = () => {
-    htmlsaida.innerHTML = criptografar(htmltexto.value, Number(htmlchave.value))
+    htmlsaida.innerHTML = cifrar(htmltexto.value, Number(htmlchave.value), "C")
 }
 
 const funcao_descriptografar = () => {
-    htmlsaida.innerHTML = descriptografar(htmltexto.value, Number(htmlchave.value))
+    htmlsaida.innerHTML = cifrar(htmltexto.value, Number(htmlchave.value), "D")
 }
 
-
-const criptografar = (texto, chave) => {
+const cifrar = (texto, chave, operacao) => {
     saida = ""
     if (chave < 1 || chave > 94){
         return "ERRO"
     }
     for (let letra of texto){
         codigo = letra.charCodeAt()
-        novoCodigo = codigo + chave
-        if (novoCodigo > 126){
-            novoCodigo = novoCodigo - 95
+        if (operacao == "C"){
+            novoCodigo = codigo + chave
+            if (novoCodigo > 126){
+                novoCodigo = novoCodigo - 95
+            }
+        }
+        else if (operacao == "D"){
+            novoCodigo = codigo - chave
+            if (novoCodigo < 32){
+                novoCodigo = novoCodigo + 95
+            }
+        }
+        else {
+            return "ERRO"
         }
         novaLetra = String.fromCharCode(novoCodigo)
         saida = saida + novaLetra
     }
-    // Aqui, a saída já está pronta
-    return saida
-}
-
-const descriptografar = (texto, chave) => {
-    saida = ""
-    if (chave < 1 || chave > 94){
-        return "ERRO"
-    }
-    for (let letra of texto){
-        codigo = letra.charCodeAt()
-        novoCodigo = codigo - chave
-        if (novoCodigo < 32){
-            novoCodigo = novoCodigo + 95
-        }
-        novaLetra = String.fromCharCode(novoCodigo)
-        saida = saida + novaLetra
-    }
-    // Aqui, a saída já está pronta
-    return saida
+        return saida
 }
